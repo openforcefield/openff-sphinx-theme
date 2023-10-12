272aac67671f5afffb7671849ffe3b589ef3d206
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-10-12 14:45:11 +1100

    Add only to header for debugging

--- openff_sphinx_theme/openff_sphinx_theme/static/openff.js
+++ openff_sphinx_theme/openff_sphinx_theme/static/openff.js
@@ -67,7 +67,7 @@ function initRTDObserver() {
       if (mutation.addedNodes[0].data.search("Inserted RTD Footer") != -1) {
         mutation.addedNodes.forEach((node) => {
           document.getElementById("rtd-versions-container-header").append(node);
-          document.getElementById("rtd-versions-container-burger").append(node);
+          // document.getElementById("rtd-versions-container-burger").append(node);
         });
       }
     });
